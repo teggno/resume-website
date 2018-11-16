d8c8715589d0f5fccbed24d462bc2b449dfbafe2
Christian Bär <teggno@hotmail.com>
2018-11-16 11:22:43 +0100

    Some more variables for styles

--- src/css.ts
+++ src/css.ts
@@ -1,5 +1,6 @@
 const fontFamily = "sans-serif";
 const fontColor = "dark-gray";
+const normalFontSize = "f6 f5-ns"
 
 const nearWhite = "near-white";
 const stripedBackground = `striped--${nearWhite}`;
@@ -8,30 +9,40 @@ const nearWhiteBackground = `bg-${nearWhite}`;
 const lightSilver = "light-silver";
 const lightSilverBorder = `b--${lightSilver}`;
 
+const lightBlue = "light-blue";
+const lightBlueBackground = `bg-${lightBlue}`;
+
+const lightestBlue = "lightest-blue";
+const lightestBlueBackground = `bg-${lightestBlue}`;
+
+const blue = "blue";
+const blueBackground = `bg-${blue}`;
+
+
 export const componentSpacingVertical = "mr4";
 export const link = `${fontColor} link hover-blue`;
 export const dt = "fw6";
 export const dd = "ma0 mb2";
 
 const button = "f6 link br1 ph3 pv2 mb2 dib white mh2";
-export const normalButton = `${button} bg-blue`;
-export const clickedButton = `${button} bg-light-blue`;
+export const normalButton = `${button} ${blueBackground}`;
+export const clickedButton = `${button} ${lightBlueBackground}`;
 
 export const checkboxListItem = "nowrap";
 
-export const stripedStringList = `lh-copy pa2 ${stripedBackground}`;
-export const textParagraph = "f6 f5-ns lh-copy measure";
+export const textParagraph = `${normalFontSize} lh-copy measure`;
 export const list = "list pa0";
+export const stripedStringList = `lh-copy pa2 ${stripedBackground}`;
 export const card = `br3 hidden ba ${lightSilverBorder}`;
-export const cardTitle = `${nearWhiteBackground} f4 br3 br--top black-60 mv0 pv2 ph3`;
+export const cardTitle = `${nearWhiteBackground} f4 br3 br--top mv0 pv2 ph3`;
 /** Container for the content of the card below the title bar. */
 export const cardContent = "pa3 bt b--black-10";
 
 export const skillList = `relative bt pa2 ${lightSilverBorder}`;
-export const skillListBar = "absolute top-0 left-0 bg-lightest-blue";
+export const skillListBar = `absolute top-0 left-0 ${lightestBlueBackground}`;
 export const skillLinkTitle = "f4 fw6";
-export const skillLinkSub = "f6";
+export const skillLinkSub = `${normalFontSize}`;
 export const wrappingList = `${list} cf`;
 export const wrappingListItem = "fl mr4 mb4 w-100 w-40-l";
 
-export const mainContainer = `${fontFamily} ${fontColor} pv2`;
+export const mainContainer = `${fontFamily} ${fontColor} ${normalFontSize} pv2`;
