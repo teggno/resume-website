2cdb8a5df456f2994e2c1095b181c5decc30bc0d
Christian Bär <teggno@hotmail.com>
2018-11-18 15:11:11 +0100

    Output index.html to dist too. Load some scripts from CDN in production.

--- webpack.development.js
+++ webpack.development.js
@@ -1,16 +1,30 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    publicPath: "/dist/",
     compress: true,
     port: 8080,
     host: "127.0.0.1",
     headers: {
       "Access-Control-Allow-Origin": "*"
     }
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      templateParameters: {
+        scripts: {
+          react: "./node_modules/react/umd/react.development.js",
+          reactDom: "./node_modules/react-dom/umd/react-dom.development.js",
+          smoothScroll:
+            "./node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js"
+        }
+      }
+    })
+  ]
 });
--- webpack.production.js
+++ webpack.production.js
@@ -1,7 +1,22 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "source-map"
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+      templateParameters: {
+        scripts: {
+          react: "https://unpkg.com/react@16/umd/react.production.min.js",
+          reactDom:
+            "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js",
+          smoothScroll: "https://npmcdn.com/smoothscroll"
+        }
+      }
+    })
+  ]
 });
