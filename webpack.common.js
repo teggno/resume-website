4e7e74d2e4fa3518c8c4c16b813ccdd066397c7e
Christian Bär <teggno@hotmail.com>
2018-12-04 09:27:21 +0100

    Add timestamp to bundle.js filename for cache busting.

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    filename: "bundle.js",
+    filename: "bundle" + new Date().valueOf() + ".js",
     path: __dirname + "/dist"
   },
 
