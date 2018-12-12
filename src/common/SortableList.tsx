72d0fa2aaa8dfa4bbf754813257358709e588882
Christian Bär <teggno@hotmail.com>
2018-12-12 12:12:06 +0100

    Don't crash when no sortConfigs passed to SortableList.

--- src/common/SortableList.tsx
+++ src/common/SortableList.tsx
@@ -3,26 +3,37 @@ import ButtonList from "../common/ButtonList";
 import { list } from "../css";
 import SparklineListItem from "../common/SparklineListItem";
 import ListItem from "../common/ListItem";
+import { find, isEmpty } from "ramda";
 
 export default class SortableList<TItem> extends React.Component<
   SortableListProps<TItem>,
-  { sort: string }
+  { sort: string | null }
 > {
   constructor(props: SortableListProps<TItem>) {
     super(props);
 
     this.state = {
-      sort: props.sortConfigs[0].name
+      sort: isEmpty(props.sortConfigs) ? null : props.sortConfigs[0].name
     };
 
     this.sortChanged = this.sortChanged.bind(this);
   }
 
   render() {
-    const button = this.props.sortConfigs.filter(
-        b => b.name === this.state.sort
-      )[0],
-      sprk = button.sparkline ? button.sparkline(this.props.items) : null;
+    const sortConfig = find(
+      b => b.name === this.state.sort,
+      this.props.sortConfigs
+    );
+
+    return sortConfig
+      ? this.renderWithConfig(sortConfig)
+      : "Cannot render list without config.";
+  }
+
+  private renderWithConfig(sortConfig: SortConfig<TItem>) {
+    const sparkline = sortConfig.sparkline
+      ? sortConfig.sparkline(this.props.items)
+      : null;
     return (
       <>
         <ButtonList
@@ -30,30 +41,30 @@ export default class SortableList<TItem> extends React.Component<
             name: c.name,
             label: c.buttonLabel
           }))}
-          value={this.state.sort}
+          value={sortConfig.name}
           changed={this.sortChanged}
         />
         <ul className={list}>
-          {button
+          {sortConfig
             .sort(this.props.items)
             .map(i =>
-              sprk ? (
+              sparkline ? (
                 <SparklineListItem
-                  title={button.itemTitle(i)}
-                  sub={button.itemSub(i)}
+                  title={sortConfig.itemTitle(i)}
+                  sub={sortConfig.itemSub(i)}
                   href={this.props.href(i)}
-                  chartMin={sprk.chartMin}
-                  barFrom={sprk.barFrom(i)}
-                  barTo={sprk.barTo(i)}
-                  chartMax={sprk.chartMax}
-                  key={button.itemTitle(i)}
+                  chartMin={sparkline.chartMin}
+                  barFrom={sparkline.barFrom(i)}
+                  barTo={sparkline.barTo(i)}
+                  chartMax={sparkline.chartMax}
+                  key={sortConfig.itemTitle(i)}
                 />
               ) : (
                 <ListItem
-                  title={button.itemTitle(i)}
-                  sub={button.itemSub(i)}
+                  title={sortConfig.itemTitle(i)}
+                  sub={sortConfig.itemSub(i)}
                   href={this.props.href(i)}
-                  key={button.itemTitle(i)}
+                  key={sortConfig.itemTitle(i)}
                 />
               )
             )}
@@ -72,19 +83,21 @@ export default class SortableList<TItem> extends React.Component<
 interface SortableListProps<TItem> {
   items: TItem[];
   href: (item: TItem) => string;
-  sortConfigs: {
-    buttonLabel: string;
-    name: string;
-    sort: (items: TItem[]) => TItem[];
-    itemTitle: (item: TItem) => string;
-    itemSub: (item: TItem) => string;
-    sparkline?: (
-      items: TItem[]
-    ) => {
-      chartMin: number;
-      barFrom: (item: TItem) => number;
-      barTo: (item: TItem) => number;
-      chartMax: number;
-    };
-  }[];
+  sortConfigs: SortConfig<TItem>[];
+}
+
+interface SortConfig<T> {
+  buttonLabel: string;
+  name: string;
+  sort: (items: T[]) => T[];
+  itemTitle: (item: T) => string;
+  itemSub: (item: T) => string;
+  sparkline?: (
+    items: T[]
+  ) => {
+    chartMin: number;
+    barFrom: (item: T) => number;
+    barTo: (item: T) => number;
+    chartMax: number;
+  };
 }
